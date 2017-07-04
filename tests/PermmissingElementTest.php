81cd760a3da733c4cd47dca2d9446765e00f671e
Tomo Šala <hcs.omot@gmail.com>
2017-07-05 00:41:37 +0200

    Solve Time Complexity | task 2: PermmissingElement
    Used simple array functions to find the difference between expected and
    provided data.

---
+++ tests/PermmissingElementTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace HcsOmot\Codility\Tests;
+
+use HcsOmot\Codility\Lessons\TimeComplexity\PermmissingElement;
+use PHPUnit\Framework\TestCase;
+
+class PermmissingElementTest extends TestCase
+{
+    /**
+     * @dataProvider provideAnArrayWithMissingElement10
+     * @dataProvider provideAnArrayWithMissingFirstElementOfSequence
+     * @dataProvider provideAnArrayWithMissingRandomElementOfARandomizedSequence
+     */
+    public function testPermmissingElementCanBeFound(array $arrayWithMissingElement, int $missingElement)
+    {
+        $permmissingElement = new PermmissingElement();
+
+        $this->assertEquals($missingElement, $permmissingElement->findMissingElementInArray($arrayWithMissingElement));
+    }
+
+    public function provideAnArrayWithMissingElement10(): array
+    {
+        return [
+            [[1, 2, 3, 4, 5, 6, 7, 8, 9], 10],
+        ];
+    }
+
+    public function provideAnArrayWithMissingFirstElementOfSequence(): array
+    {
+        return [
+            [[2, 3, 4, 5, 6, 7, 8, 9, 10], 1],
+        ];
+    }
+
+    public function provideAnArrayWithMissingRandomElementOfARandomizedSequence(): array
+    {
+        return [
+            [[1, 3, 6, 2, 8, 10, 5, 7, 9], 4],
+            [[4, 7, 6, 3, 9, 10, 2, 5, 1], 8],
+            [[6, 10, 4, 3, 9, 1, 7, 2, 5], 8],
+        ];
+    }
+}
