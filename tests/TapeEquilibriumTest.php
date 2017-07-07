c945fa5d42731d3ed96ecbf644eb405650f356cd
Tomo Šala <hcs.omot@gmail.com>
2017-07-07 15:27:58 +0200

    Solve for extremely small input sets
    This is a workig solution, but extremely suboptimal.
    
    See evaluation details here:
     https://codility.com/demo/results/training85S8WX-54T/
    
    This requires a rewrite based on thorough analysis.

---
+++ tests/TapeEquilibriumTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace HcsOmot\Codility\Tests;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use HcsOmot\Codility\Lessons\TimeComplexity\TapeEquilibrium;
+use PHPUnit\Framework\TestCase;
+
+class TapeEquilibriumTest extends TestCase
+{
+    /**
+     * @dataProvider provideArrayAndMinimalDifference
+     */
+    public function testFindMinimalDifferenceForArray(array $array, int $expectedMinimalDifference)
+    {
+        $tapeEquilibriumFinder = new TapeEquilibrium();
+
+        $this->assertEquals($expectedMinimalDifference, $tapeEquilibriumFinder->findMinimalDifference($array));
+    }
+
+    public function provideArrayAndMinimalDifference()
+    {
+        return [
+            [[1, 2, 3, 4, 5], 3],
+            [[3, 1, 2, 4, 3], 1],
+        ];
+    }
+}
